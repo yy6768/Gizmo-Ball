3ae55b6802a9752425cbe4ae85cb87be96934b83
yy6768 <102475442+yy6768@users.noreply.github.com>
2022-11-08 21:05:44 +0800

    Revert "web前端界面更新"

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,26 @@
 <template>
-  <router-view/>
+    <router-view/>
 </template>
-
-<script>
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap"
-
-export default {
-  name: 'app',
-  
-}
-</script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
